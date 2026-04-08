ba0ff56b6827c9cf774aac108bc868d2f5396c67
Aditya Dhumal <119112846+adhumal007@users.noreply.github.com>
2026-04-07 22:21:52 -0400

    Add author information to load_fema_data.py
    Added author information to the script header.

--- etl/load_fema_data.py
+++ etl/load_fema_data.py
@@ -5,6 +5,7 @@ Usage:
     1. Copy ../.env.example to ../.env and fill in your Supabase/PostgreSQL credentials
     2. pip install -r requirements.txt
     3. python load_fema_data.py
+Author: Tanmay, Aditya
 """
 
 import os
